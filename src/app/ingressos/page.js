41415fa3e2eda1e8d0c3226e26c2e9d60029e5c2
Marcos <marcos.ferreira@aluno.ufr.edu.br>
2024-04-22 19:51:37 -0400

    update

--- src/app/ingressos/page.js
+++ src/app/ingressos/page.js
@@ -39,7 +39,6 @@ export default function UserForm() {
           setErrorMessage('Erro ao obter dados. Por favor, tente novamente.');
         } finally {
           await prisma.$disconnect();
-          console.log("teste")
         }
       }
     };
